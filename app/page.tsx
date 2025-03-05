1f62e7642052237bc5ce1790500d5ce6a5a70786
moha <25137956+FunnyPhantom@users.noreply.github.com>
2025-03-05 18:29:53 +0100

    feat: add images, add initial description

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
 import Image from "next/image";
+import luckImage from "./images/luck.png";
+import dichotomyLoveTruthImage from "./images/dichotomy-love-truth.png";
 
 
 export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
         <h1 className={"text-amber-300 opacity-25 font-black text-4xl"}>Moha Movahedmanesh</h1>
-
-        {/* <Image src={"/images/luck.png"} alt={"Luck"} width={100} height={100} /> */}
+        
+      </div>
+      <div className="grid grid-cols-2 gap-8 ">
+        <Image src={luckImage} alt={"Luck"} width={300} height={300} />
+        <Image src={dichotomyLoveTruthImage} alt={"Dichotomy Love Truth"} width={300} height={300} />
+      </div>
+      <div>
+        <p>
+          {"I'm a software engineer with a passion for building products that help people live better lives."}
+        </p>
       </div>
     </div>
   );
